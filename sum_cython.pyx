f905206099da8442b97715f90909bb7d27f9557b
tvg209 <tranvangiai2009@gmail.com>
2026-04-06 10:48:02 +0200

    Update sum_cython.pyx

--- sum_cython.pyx
+++ sum_cython.pyx
@@ -1,7 +1,16 @@
 
+# cython: boundscheck=False, wraparound=False, initializedcheck=False, cdivision=True
+# distutils: extra_compile_args = -O3 -march=native -fopenmp
+# distutils: extra_link_args = -fopenmp
+
+from cython.parallel cimport prange
+
 def sum_cython(double[:] a):
     cdef Py_ssize_t i, n = a.shape[0]
-    cdef double s = 0
-    for i in range(n):
+    cdef double s = 0.0
+    n = a.shape[0]
+
+    for i in prange(n, nogil=True, schedule='static'):
         s += a[i]
+
     return s
