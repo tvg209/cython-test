7c0ceb048ca390e54cb5fd5f202fa6d0b1543517
tvg209 <tranvangiai2009@gmail.com>
2026-04-06 11:00:52 +0200

    Update sum_cython.pyx

--- sum_cython.pyx
+++ sum_cython.pyx
@@ -1,4 +1,5 @@
 
+
 # cython: boundscheck=False, wraparound=False, initializedcheck=False, cdivision=True
 # distutils: extra_compile_args = -O3 -march=native -fopenmp
 # distutils: extra_link_args = -fopenmp
@@ -8,9 +9,15 @@ from cython.parallel cimport prange
 def sum_cython(double[:] a):
     cdef Py_ssize_t i, n = a.shape[0]
     cdef double s = 0.0
+    cdef double tmp = 0.0
     n = a.shape[0]
 
+    # On utilise une variable locale PAR THREAD :
+    # tmp sera séparé pour chaque thread (important !)
     for i in prange(n, nogil=True, schedule='static'):
-        s += a[i]
+        tmp += a[i]
+
+    # Réduction finale après la boucle parallèle
+    s += tmp
 
     return s
