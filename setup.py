2dbc41aef1ac90e92090c2cabce4b56dd6b6c1f3
tvg209 <tranvangiai2009@gmail.com>
2026-04-06 11:36:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ ext = Extension(
     name="banded_solver",
     sources=["banded_solver.pyx"],
     include_dirs=[numpy.get_include()],
-    extra_compile_args=["-O3", "-march=native", "-fopenmp"],
-    extra_link_args=["-fopenmp"],
+    extra_compile_args=["/O2"],
 )
 
 setup(
-    ext_modules=cythonize(ext, language_level="3", annotate=False),
+    ext_modules=cythonize(ext, language_level="3"),
 )
